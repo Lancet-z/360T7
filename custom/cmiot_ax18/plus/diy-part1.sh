220fae4b30580f923ab8ccd8b4a7ae6bc80f2e5f
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-08 10:33:20 +0800

    Update diy-part1.sh

--- custom/cmiot_ax18/plus/diy-part1.sh
+++ custom/cmiot_ax18/plus/diy-part1.sh
@@ -13,6 +13,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#openclash
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#主题列表
+echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
