4c23dc9a154afd74d314b943f9d8fa587b74fd27
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-11-28 19:33:44 +0800

    Update diy-part1.sh

--- custom/cmiot_ax18/plus/diy-part1.sh
+++ custom/cmiot_ax18/plus/diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #openclash
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-#主题列表
-echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
+echo 'src-git-full openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+#argon主题
+echo 'src-git-full jerryk https://github.com/jerrykuku/openwrt-package' >> feeds.conf.default
