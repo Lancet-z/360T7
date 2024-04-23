35a70c9c8d50eb52577498b2513492123aa9c025
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2024-04-23 21:51:23 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
