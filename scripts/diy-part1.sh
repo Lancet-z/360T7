112ba86d5431b7b911e32543f7202c031e4e6d69
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2024-04-23 21:52:42 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -20,4 +20,5 @@ sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
+./scripts/feeds install -a 
 #加主题
